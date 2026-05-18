24fd4c9fdc7de089dd97b516737cfb659dc7b412
Abhishek Suryawanshi <suryawanshi1260@gmail.com>
2026-05-18 23:21:14 +0530

    Change Division program to use integers
    Changed variable types from float to int and updated input methods accordingly.

--- Division.java
+++ Division.java
@@ -6,15 +6,16 @@ class Division
     {
         Scanner sobj = new Scanner(System.in);
 
-        float No1 = 0.0f, No2 = 0.0f, Ans = 0.0f;
+        int No1 = 0, No2 = 0, Ans = 0;
 
         System.out.println("Enter first number : ");
-        No1 = sobj.nextFloat();
+        No1 = sobj.nextInt();
 
         System.out.println("Enter second number : ");
-        No2 = sobj.nextFloat();
+        No2 = sobj.nextInt();
 
         Ans = No1 / No2;
 
         System.out.println("Division is : "+Ans);
     }
+}
